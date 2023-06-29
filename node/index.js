3ffc638240d325edebc087ea272287b5f59b5327
dieison-de-moura <di.eison95@outlook.com>
2023-06-29 17:16:23 -0300

    alterações nos dockers, lista das pessoas

--- node/index.js
+++ node/index.js
@@ -1,23 +1,30 @@
 const express = require('express')
+const { queryPromise } = require('./queryPromise')
 const app = express()
 const port = 3000
 
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database: 'nodedb'
-// };
+app.get('/', async (req, res) => {
+    const sqlCreateTable = `CREATE TABLE IF NOT EXISTS people(id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))`;
+    await queryPromise.query(sqlCreateTable)
 
-// const mysql = require('mysql2')
-// const connection = mysql.createConnection(config)
+    const people = [['Dieison'], ['Lucian'], ['Wesley'], ['Luiz'], ['Gabriel']]
+    const sqlInsert = `INSERT INTO people(name) VALUES ?`
 
-// const sql = `INSERT INTO people(name) values('lucian')`
-// connection.query(sql)
-// connection.end()
+    await queryPromise.queryMultiple(sqlInsert, people)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    const sqlPeople = `SELECT * FROM people`
+    const allPeople = await queryPromise.query(sqlPeople)
+
+    const html = `<h1>Full Cycle Rocks!</h1>\n
+    <h2>List of people!</h2>\n
+  <ul>
+    ${allPeople.map(people => `<li>${people.name}</li>`).join('')}
+  </ul>`
+
+    const dropTable = `DROP TABLE IF EXISTS people`;
+    await queryPromise.query(dropTable)
+
+    res.send(html)
 })
 
 app.get('/oi', (req, res) => {
